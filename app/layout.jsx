724e0f5fb7995bcc8154b7334b3fad0693918247
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-12-30 11:11:50 +0300

    Refactor code structure for improved readability and maintainability

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,24 +1,24 @@
-import { Outfit } from "next/font/google";
+import { SUSE } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import StoreProvider from "@/app/StoreProvider";
 import "./globals.css";
 
-const outfit = Outfit({ subsets: ["latin"], weight: ["400", "500", "600"] });
+const suse = SUSE({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata = {
-    title: "GoCart. - Shop smarter",
-    description: "GoCart. - Shop smarter",
+  title: "Aura Cosmetic Market",
+  description: "Shop smarter, faster & cheaper",
 };
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-            <body className={`${outfit.className} antialiased`}>
-                <StoreProvider>
-                    <Toaster />
-                    {children}
-                </StoreProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className={`${suse.className} antialiased`}>
+        <StoreProvider>
+          <Toaster />
+          {children}
+        </StoreProvider>
+      </body>
+    </html>
+  );
 }
